9dc4240db692fc43287eeeabc6316df163d49cac
John Haven Bradley <johnhavenbradley@gmail.com>
2018-04-16 19:08:14 -0700

    update

---
+++ express/DeveloperMarketplace/src/app/pages/home/home-team-leads/home-team-leads.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-team-leads',
+  templateUrl: './home-team-leads.component.html',
+  styleUrls: ['./home-team-leads.component.css']
+})
+export class HomeTeamLeadsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- express/DeveloperMarketplace/src/app/pages/home/portfolio-container/portfolio/portfolio.component.ts
+++ express/DeveloperMarketplace/src/app/pages/home/portfolio-container/portfolio/portfolio.component.ts
@@ -1,5 +1,5 @@
 import { Component, NgModule, OnInit } from '@angular/core';
-import { HttpService} from './../../.././services/http.service';
+import { HttpService} from './../../../.././services/http.service';
 
 @Component({
   selector: 'app-portfolio',
